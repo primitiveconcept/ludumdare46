0233b5d449848fb9bac04471ac8663429941ec57
David Edmondson <threehams@gmail.com>
2020-04-17 23:50:34 -0700

    JSON fixture for INITIAL_STATE

--- server/HackThePlanet/Endpoints/EchoEndpoint.cs
+++ server/HackThePlanet/Endpoints/EchoEndpoint.cs
@@ -1,14 +1,14 @@
 namespace HackThePlanet
 {
-	using WebSocketSharp;
-	
-	
-	public class EchoEndpoint : WebsocketEndpoint
-	{
-		protected override void OnMessage(MessageEventArgs message)
-		{
-			Send("Echo: " + message.Data);
-		}
-	}
-	
+  using WebSocketSharp;
+
+
+  public class EchoEndpoint : WebsocketEndpoint
+  {
+    protected override void OnMessage(MessageEventArgs message)
+    {
+      Send("{ \"type\": \"INITIAL_STATE\", \"payload\": { \"nodeCount\": 0, \"bitcoin\": 1, \"availableCommands\": [\"SSH\"], \"knownDevices\": [{ \"ip\": \"199.201.159.1\" }], \"message\": \"Welcome\"}}");
+    }
+  }
+
 }
