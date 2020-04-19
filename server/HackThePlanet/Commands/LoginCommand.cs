70e7a5e2099aaad3391c9d9f965c856c97b940e5
Zachary A. White <zallenwhite@gmail.com>
2020-04-19 12:26:37 -0700

    Set player name on first login. Expand successful login message to imply SSH key was used.

--- server/HackThePlanet/Commands/LoginCommand.cs
+++ server/HackThePlanet/Commands/LoginCommand.cs
@@ -8,15 +8,29 @@ namespace HackThePlanet
 	{
 		public override string Execute(WebsocketEndpoint connection)
 		{
+			GameEndpoint gameEndpoint = connection as GameEndpoint;
 			string name = GetArgument(0);
-			GetDeviceStates(connection);
-			return $"Logged in as {name}";
+			
+			// First time login
+			if (string.IsNullOrEmpty(gameEndpoint.PlayerComponent.Name))
+			{
+				// Must provide a name the first time.
+				if (string.IsNullOrEmpty(name))
+					return $"Invalid login attempt";
+				
+				// Set player name.
+				else
+					gameEndpoint.PlayerComponent.Name = name;
+			}
+			
+			GetDeviceStates(gameEndpoint);
+			return $"Authenticating with public key \"imported-133+ssh-key\"\n"
+					+ $"Logged in as {name}";
 		}
 
 
-		private void GetDeviceStates(WebsocketEndpoint connection)
+		private void GetDeviceStates(GameEndpoint gameEndpoint)
 		{
-			GameEndpoint gameEndpoint = connection as GameEndpoint;
 			DeviceUpdateMessage deviceUpdateMessage = DeviceUpdateMessage.Create(gameEndpoint.PlayerEntity.NetworkAccessComponent());
 			gameEndpoint.PlayerComponent.MessageQueue.Add(deviceUpdateMessage.ToString());
 		}
