6fdce96453d47a7953765de5a324a9f3b711bd29
David Edmondson <threehams@gmail.com>
2020-04-19 13:06:51 -0700

    Basic UI tests

--- client/cypress/integration/setup.spec.ts
+++ client/cypress/integration/setup.spec.ts
@@ -7,6 +7,11 @@ describe("adjustments", () => {
     it("logs in and shows a device list", () => {
       cy.findByLabelText("Enter Username").type(`threehams{enter}`);
       cy.findByText("Logged in as threehams");
+      cy.findByText("Known Devices");
+      cy.getId({ name: "knownIp", index: 0 }).click();
+      cy.findByText("Known Devices").should("not.exist");
+      cy.findByText("Back").click();
+      cy.findByText("Known Devices");
     });
 
     it("connects to a remote machine and saves the username", () => {
