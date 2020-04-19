5178dfb1a4ed9dbfa3f2a594d7bd3649dc6cb0e6
Zachary A. White <zallenwhite@gmail.com>
2020-04-18 18:29:17 -0700

    Fix merge conflict lines.

--- server/build.sh
+++ server/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 dotnet build HackThePlanet.Host -c Release
-=======
-dotnet build
->>>>>>> 9120d72f1cbd9bfb5a2bf5523c44cf0d7c22fa25
--- server/run.sh
+++ server/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 dotnet run HackThePlanet.Host
-=======
-dotnet run --project ./HackThePlanet.Host
->>>>>>> 9120d72f1cbd9bfb5a2bf5523c44cf0d7c22fa25
+
