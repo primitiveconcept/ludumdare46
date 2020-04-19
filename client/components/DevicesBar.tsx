727006860025ca9524128d24734e5d17fe5973eb
David Edmondson <threehams@gmail.com>
2020-04-19 15:37:51 -0700

    gross, regex

--- client/components/DevicesBar.tsx
+++ client/components/DevicesBar.tsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { Markdown } from "./Markdown";
 import { State } from "../types/State";
 
+type Category = "install";
 type DevicesBarProps = {
   devices: State["devices"];
 };
 export const DevicesBar = ({ devices }: DevicesBarProps) => {
   const [currentIp, setCurrentIp] = useState<string | null>(null);
+  const [currentCategory, setCurrentCategory] = useState<Category | null>(null);
   if (!devices.length) {
     return null;
   }
@@ -14,54 +16,115 @@ export const DevicesBar = ({ devices }: DevicesBarProps) => {
     return (
       <>
         <div>Known Devices</div>
-        <ul>
-          {devices.map((device) => {
-            return (
-              <li key={device.ip}>
-                <a
-                  href={device.ip}
-                  data-test="knownIp"
-                  onClick={(event) => {
-                    event.preventDefault();
-                    setCurrentIp(device.ip);
-                  }}
-                >
-                  {device.ip}
-                </a>
-                <div>&nbsp;{device.status}</div>
-                <div>&nbsp;</div>
-              </li>
-            );
-          })}
-        </ul>
+        <br />
+        {devices.map((device) => {
+          return (
+            <div key={device.ip}>
+              <a
+                href={device.ip}
+                data-test="knownIp"
+                onClick={(event) => {
+                  event.preventDefault();
+                  setCurrentIp(device.ip);
+                }}
+              >
+                {device.ip}
+              </a>
+              <div>&nbsp;{device.status}</div>
+              <div>&nbsp;</div>
+            </div>
+          );
+        })}
       </>
     );
   }
 
   const device = devices.find((dev) => dev.ip === currentIp)!;
-  return (
-    <>
-      <div>{device.ip}</div>
-      <ul>
-        {device.commands.map((command) => {
+  const commands = {
+    main: device.commands.filter((command) => {
+      console.log(command);
+      console.log(baseCommand(command));
+      return baseCommand(command) !== "install";
+    }),
+    install: device.commands.filter((command) => {
+      return baseCommand(command) === "install";
+    }),
+  };
+
+  if (currentCategory) {
+    return (
+      <>
+        <div>{device.ip}</div>
+        {commands[currentCategory].map((command) => {
           return (
-            <li key={command}>
+            <div key={command}>
               <Markdown>{command}</Markdown>
-            </li>
+            </div>
           );
         })}
-        <li>
+        <div>
           <a
             href="back"
             onClick={(event) => {
               event.preventDefault();
-              setCurrentIp(null);
+              setCurrentCategory(null);
             }}
           >
             Back
           </a>
-        </li>
-      </ul>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div>{device.ip}</div>
+      {commands.main.map((command) => {
+        return (
+          <div key={command}>
+            <Markdown>{command}</Markdown>
+          </div>
+        );
+      })}
+      {!!commands.install.length && (
+        <div>
+          <a
+            href="install"
+            onClick={(event) => {
+              event.preventDefault();
+              setCurrentCategory("install");
+            }}
+          >
+            Install Malware
+          </a>
+        </div>
+      )}
+      <div>
+        <a
+          href="back"
+          onClick={(event) => {
+            event.preventDefault();
+            setCurrentIp(null);
+          }}
+        >
+          Back
+        </a>
+      </div>
     </>
   );
+};
+
+// copy/paste from https://github.com/probablyup/markdown-to-jsx/blob/master/index.js
+// pfffffft this is awful, but it's quicker than changing the backend response!
+const linkRegex = /\[(.+)\]\(([^ ]+?)( "(.+)")?\)/;
+
+const baseCommand = (markdown: string) => {
+  const match = linkRegex.exec(markdown);
+  if (!match) {
+    throw new Error(
+      `Received a device command with no valid Markdown link: ${markdown}`,
+    );
+  }
+  return match[2].split("|")[0];
 };
