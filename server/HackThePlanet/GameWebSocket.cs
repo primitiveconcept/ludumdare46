d129d0f5c458fa24323925df07bb33751e9bd2ca
Zachary A. White <zallenwhite@gmail.com>
2020-04-22 21:48:15 -0700

    Enable html/js file serving, out of wwwroot folder. For use in serving "dark web" files only, don't keep client files here.

--- server/HackThePlanet/GameWebSocket.cs
+++ server/HackThePlanet/GameWebSocket.cs
@@ -1,9 +1,13 @@
 namespace HackThePlanet
 {
+	using System.Net;
+	using System.Text;
 	using Microsoft.Extensions.Logging;
 	using WebSocketSharp.Server;
+	using HttpListenerRequest = WebSocketSharp.Net.HttpListenerRequest;
+	using HttpListenerResponse = WebSocketSharp.Net.HttpListenerResponse;
 
-	
+
 	/// <summary>
 	/// A wrapper around the web socket server, to inject Game state reference into each.
 	/// Also handles starting and stopping of the server.
@@ -26,8 +30,11 @@ namespace HackThePlanet
 				new HttpServer(31337) 
 					{ 
 						ReuseAddress = true,
-						KeepClean = false
+						KeepClean = false, 
+						DocumentRootPath = "wwwroot"
 					};
+
+			this.webSocketServer.OnGet += WebSocketServer_OnGet;
 		}
 		#endregion
 
@@ -65,6 +72,38 @@ namespace HackThePlanet
 		{
 			logger.LogInformation("Stopping game web socket server.");
 			this.webSocketServer.Stop();
+		}
+
+
+		private void WebSocketServer_OnGet(object sender, HttpRequestEventArgs e)
+		{
+			HttpListenerRequest request = e.Request;
+			HttpListenerResponse response = e.Response;
+
+			string path = request.RawUrl;
+			if (path == "/")
+				path += "index.html";
+
+			byte[] contents;
+			if (!e.TryReadFile(path, out contents))
+			{
+				response.StatusCode = (int)HttpStatusCode.NotFound;
+				return;
+			}
+
+			if (path.EndsWith(".html"))
+			{
+				response.ContentType = "text/html";
+				response.ContentEncoding = Encoding.UTF8;
+			}
+			else if (path.EndsWith(".js"))
+			{
+				response.ContentType = "application/javascript";
+				response.ContentEncoding = Encoding.UTF8;
+			}
+
+			response.ContentLength64 = contents.LongLength;
+			response.Close(contents, true);
 		}
 	}
 }
