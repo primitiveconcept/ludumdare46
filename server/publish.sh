c006c1b051e632ebcba248daa81a8463010963ca
Zachary A. White <zallenwhite@gmail.com>
2020-05-16 17:44:00 -0700

    You get a sudo, and you get a sudo, everyone gets a sudo.

--- server/publish.sh
+++ server/publish.sh
@@ -1,5 +1,5 @@
 sudo systemctl stop hacktheplanet.service
 sudo cp ./hacktheplanet.service /etc/systemd/system/hacktheplanet.service
 sudo systemctl daemon-reload
-dotnet publish HackThePlanet.Host --configuration Release --output /usr/local/sbin/hacktheplanet
+sudo dotnet publish HackThePlanet.Host --configuration Release --output /usr/local/sbin/hacktheplanet
 sudo systemctl start hacktheplanet.service
