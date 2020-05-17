edf719f50e63322352cbbb85335fd7c6a5ee471a
Zachary A. White <zallenwhite@gmail.com>
2020-05-16 17:32:48 -0700

    Fix some publish calls.

--- server/publish.sh
+++ server/publish.sh
@@ -1,5 +1,4 @@
 dotnet publish HackThePlanet.Host --configuration Release --output /usr/local/sbin/hacktheplanet
-systemctl stop hacktheplanet.service
-cp ./hacktheplanet.service /etc/systemd/system/hacktheplanet.service
-systemctl daemon-reload
-systemctl start hacktheplanet.service
+sudo cp ./hacktheplanet.service /etc/systemd/system/hacktheplanet.service
+sudo systemctl daemon-reload
+sudo systemctl restart hacktheplanet.service
